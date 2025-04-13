7ab6679852335ad9b6a254029e1704d14aee63dd
luqman jaffary <luqmanjaffary79@gmail.com>
2025-04-13 16:03:31 +0500

    create the new project and files

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryPink = Color(0xFFF5A8B0);
+  static const Color orangeGradientStart = Color(0xFFFF6F61);
+  static const Color orangeGradientEnd = Color(0xFFFFA726);
+  static const Color white = Colors.white;
+  static const Color grey = Colors.grey;
+  static const Color black = Colors.black;
+}
